8e7198e8d5844737e30caacc3c8e1965def6c9c9
Henrique Francisco de Souza <henriquesouza432@outlook.com>
2024-07-01 10:40:10 -0300

    config nodejs concurrently with react app

--- server.js
+++ server.js
@@ -2,13 +2,24 @@ require('dotenv/config')
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
-const PORT = process.env.API_PORT;
 
 app.use(cors());
 app.use(express.json());
 
+// Define a porta do servidor
+const PORT = process.env.PORT || 5000;
+
+// Middleware para servir o aplicativo React em produção
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.get('/api/list-cars', async (req, res) => {
   try {
