c58019126ab8bfd43c3c485c398ae46da2dd0802
Henrique Francisco de Souza <henriquesouza432@outlook.com>
2024-07-01 09:00:34 -0300

    fixing port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.API_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
