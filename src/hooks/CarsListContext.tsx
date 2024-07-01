c899c620ab0ee21897e1e0573362fdffe621863d
Henrique Francisco de Souza <henriquesouza432@outlook.com>
2024-07-01 09:14:30 -0300

    fix

--- src/hooks/CarsListContext.tsx
+++ src/hooks/CarsListContext.tsx
@@ -43,7 +43,7 @@ export const Provider = ({ children }: Props) => {
       }
 
       try {
-        const response = await axios.get('/api/list-cars');
+        const response = await axios.get('/api/list-cars:5000');
         const data = response.data.cars
         const groupedCars = groupCarsByBrand(data);
         setCarsList(groupedCars);
